87baf57a356e487d1d6aa80db09f4b1aa5318338
Artem Tokar <102070379+ArtemTokar21@users.noreply.github.com>
2026-03-05 00:41:01 +0200

    Add files via upload
    Initial upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Change this line inside your forEach loop:
+row.innerHTML = `
+    <span>${item.description}</span>
+    <span>$${item.amount.toFixed(2)}</span>
+    <button class="delete-btn" onclick="removeItem(${item.id})">Delete</button>
+`;
